447d722ea48a6a6a6927ab226bde0734904c1745
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-03-04 18:55:02 +0100

    Update vaperina-panel-init.js

--- javascripts/discourse/api-initializers/vaperina-panel-init.js
+++ javascripts/discourse/api-initializers/vaperina-panel-init.js
@@ -84,22 +84,6 @@ export default {
             component.set("stinkinBadges", stinkinBadges);
           });
         }
-      });      
-      api.modifyClass("component:site-header", {
-        pluginId: "sticky-create-topic",
-        @on("didInsertElement")        
-        stickyCreateTopicrCheck() {
-          const anchor = document.querySelector(".vaperina-panel");
-          const body = document.querySelector("body");
-          if (!"IntersectionObserver" in window) return;
-          new IntersectionObserver(entries => {
-            if (!entries[0].isIntersecting) {
-              body.classList.add("sticky-create-topic");
-            } else {
-              body.classList.remove("sticky-create-topic");
-            }
-          }).observe(anchor);
-        }
       });
     });
   },
