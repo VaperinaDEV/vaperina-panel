aca75392d68e9d120c753bcb219ca647ff1ca598
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-03-05 12:36:58 +0100

    Update vp-panel.js

--- javascripts/discourse/widgets/vp-panel.js
+++ javascripts/discourse/widgets/vp-panel.js
@@ -16,6 +16,17 @@ function getUser() {
   return user;
 }
 
+function getVaperinaPanel() {
+  let pref = localStorage.getItem("vaperinaPanel");
+  let result = settings.default_enabled;
+  if (pref !== null) {
+    result = pref === "true";
+  }
+  return result;
+}
+
+if (!getVaperinaPanel()) {
+
 createWidget('vp-avatar', {
   tagName: 'div.vp-avatar',
   
@@ -87,3 +98,4 @@ createWidget('vp-topic', {
     );
   },
 });
+}
