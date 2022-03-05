045813240a79aff921c53e65715737198b37280c
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-03-05 13:13:36 +0100

    Update vaperina-panel-init.js

--- javascripts/discourse/api-initializers/vaperina-panel-init.js
+++ javascripts/discourse/api-initializers/vaperina-panel-init.js
@@ -131,101 +131,112 @@ DropdownSelectBoxComponent.extend({
   name: "vaperina-new-topic-drop",
   initialize() {
     withPluginApi("0.8.7", (api) => {
-      api.registerConnectorClass("discovery-list-container-top", "vaperina-panel", {
-        classNames: ["new-topic-dropdown-panel"],
+      if (api.getCurrentUser() === null) return false;
+      function getVaperinaPanel() {
+        let pref = localStorage.getItem("vaperinaPanel");
+        let result = settings.default_enabled;
+        if (pref !== null) {
+          result = pref === "true";
+        }
+        return result;
+      }
+      if (!getVaperinaPanel()) {
+        api.registerConnectorClass("discovery-list-container-top", "vaperina-panel", {
+          classNames: ["new-topic-dropdown-panel"],
 
-        selectKitOptions: {
-          icons: ["bolt"],
-          showFullTitle: false,
-          autoFilterable: false,
-          filterable: false,
-          showCaret: true,
-          none: "topic.create",
-        },
+          selectKitOptions: {
+            icons: ["bolt"],
+            showFullTitle: false,
+            autoFilterable: false,
+            filterable: false,
+            showCaret: true,
+            none: "topic.create",
+          },
 
-        content: computed(function () {
+          content: computed(function () {
 
-          const hideForNewUser = this.currentUser && this.currentUser.trust_level > 0;
+            const hideForNewUser = this.currentUser && this.currentUser.trust_level > 0;
 
-          const items = [
-            {
-              id: "new_question",
-              name: "Kérdésed van?",
-              description: "Ne habozz, itt mindenki szívesen segít...",
-              icon: "question-circle",
-            },
-          ];
-          items.push({
-            id: "new_comment",
-            name: "Társalgó",
-            description: "Dobj fel egy érdekes témát...",
-            icon: "comment",
-          });
-          items.push({
-            id: "new_handcheck",
-            name: "Handcheck",
-            description: "Vapemail? Na, hadd lássuk...",
-            icon: "camera",
-          });
-          if (hideForNewUser) {
+            const items = [
+              {
+                id: "new_question",
+                name: "Kérdésed van?",
+                description: "Ne habozz, itt mindenki szívesen segít...",
+                icon: "question-circle",
+              },
+            ];
             items.push({
-              id: "new_ad",
-              name: "Hirdetésfeladás",
-              description: "Hirdess gyorsan, egyszerűen...",
-              icon: "tags",
+              id: "new_comment",
+              name: "Társalgó",
+              description: "Dobj fel egy érdekes témát...",
+              icon: "comment",
             });
-          }
-          return items;
-        }),
-
-        @action
-        onChange(selectedAction) {
-
-          if (selectedAction === "new_question") {
-            const composerController = getOwner(this).lookup("controller:composer");
-            let categoryId = 49;
-
-            composerController.open({
-              action: Composer.CREATE_TOPIC,
-              draftKey: Composer.NEW_TOPIC_KEY,
-              categoryId: categoryId,
+            items.push({
+              id: "new_handcheck",
+              name: "Handcheck",
+              description: "Vapemail? Na, hadd lássuk...",
+              icon: "camera",
             });
-          }
+            if (hideForNewUser) {
+              items.push({
+                id: "new_ad",
+                name: "Hirdetésfeladás",
+                description: "Hirdess gyorsan, egyszerűen...",
+                icon: "tags",
+              });
+            }
+            return items;
+          }),
 
-          if (selectedAction === "new_comment") {
-            const composerController = getOwner(this).lookup("controller:composer");
-            let categoryId = 7;
+          @action
+          onChange(selectedAction) {
 
-            composerController.open({
-              action: Composer.CREATE_TOPIC,
-              draftKey: Composer.NEW_TOPIC_KEY,
-              categoryId: categoryId,
-            });
-          }
+            if (selectedAction === "new_question") {
+              const composerController = getOwner(this).lookup("controller:composer");
+              let categoryId = 49;
 
-          if (selectedAction === "new_handcheck") {
-            const composerController = getOwner(this).lookup("controller:composer");
-            let categoryId = 5;
+              composerController.open({
+                action: Composer.CREATE_TOPIC,
+                draftKey: Composer.NEW_TOPIC_KEY,
+                categoryId: categoryId,
+              });
+            }
 
-            composerController.open({
-              action: Composer.CREATE_TOPIC,
-              draftKey: Composer.NEW_TOPIC_KEY,
-              categoryId: categoryId,
-            });
-          }
+            if (selectedAction === "new_comment") {
+              const composerController = getOwner(this).lookup("controller:composer");
+              let categoryId = 7;
 
-          if (selectedAction === "new_ad") {
-            const composerController = getOwner(this).lookup("controller:composer");
-            let categoryId = 31;
+              composerController.open({
+                action: Composer.CREATE_TOPIC,
+                draftKey: Composer.NEW_TOPIC_KEY,
+                categoryId: categoryId,
+              });
+            }
 
-            composerController.open({
-              action: Composer.CREATE_TOPIC,
-              draftKey: Composer.NEW_TOPIC_KEY,
-              categoryId: categoryId,
-            });
-          }
-        },
-      });
+            if (selectedAction === "new_handcheck") {
+              const composerController = getOwner(this).lookup("controller:composer");
+              let categoryId = 5;
+
+              composerController.open({
+                action: Composer.CREATE_TOPIC,
+                draftKey: Composer.NEW_TOPIC_KEY,
+                categoryId: categoryId,
+              });
+            }
+
+            if (selectedAction === "new_ad") {
+              const composerController = getOwner(this).lookup("controller:composer");
+              let categoryId = 31;
+
+              composerController.open({
+                action: Composer.CREATE_TOPIC,
+                draftKey: Composer.NEW_TOPIC_KEY,
+                categoryId: categoryId,
+              });
+            }
+          },
+        });
+      }
     });
   },
 });
