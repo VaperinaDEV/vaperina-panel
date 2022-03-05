2811a4c4acd3f53f2c100d6142a98f6b5698fb4e
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-03-05 15:41:17 +0100

    Update vaperina-panel-init.js

--- javascripts/discourse/api-initializers/vaperina-panel-init.js
+++ javascripts/discourse/api-initializers/vaperina-panel-init.js
@@ -9,10 +9,10 @@ export default {
         
       if (api.getCurrentUser() === null) return false;
       
-      api.registerConnectorClass("discovery-list-container-top", "vaperina-panel", "vp-panel-contents-top", (helper) => {
+      api.registerConnectorClass("discovery-list-container-top", "vaperina-panel", (helper) => {
         return helper.attach("vp-avatar", helper);
       });
-      api.registerConnectorClass("discovery-list-container-top", "vaperina-panel", "vp-new-topic", (helper) => {
+      api.registerConnectorClass("discovery-list-container-top", "vaperina-panel", (helper) => {
         return helper.attach("vp-topic", helper);
       });
         
