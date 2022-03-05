505b43d249024288aa11f688dd0379dc72deaa09
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-03-05 16:54:06 +0100

    Update vaperina-panel-init.js

--- javascripts/discourse/api-initializers/vaperina-panel-init.js
+++ javascripts/discourse/api-initializers/vaperina-panel-init.js
@@ -6,6 +6,17 @@ export default {
   name: "vaperina-panel",
   initialize() {
     withPluginApi("0.8.7", (api) => {
+      
+      function getVaperinaPanel() {
+        let pref = localStorage.getItem("vaperinaPanel");
+        let result = settings.default_enabled;
+        if (pref !== null) {
+          result = pref === "true";
+        }
+        return result;
+      }
+  
+      if (getVaperinaPanel()) {
         
       if (api.getCurrentUser() === null) return false;
         
@@ -90,6 +101,7 @@ export default {
           });
         }
       });
+      }
     });
   },
 };
