4ea50300e562a9ebb6cb6884cade2f7112e291c5
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-03-08 18:03:25 +0100

    Update new-topic-button.js

--- javascripts/discourse/components/new-topic-button.js
+++ javascripts/discourse/components/new-topic-button.js
@@ -8,6 +8,7 @@ import { inject as service } from "@ember/service";
 
 export default Component.extend(FilterModeMixin, {
   router: service(),
+  classNames: ["new-topic-button"],
 
   tagName: "",
   
