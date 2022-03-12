4a8ffcdcc1d8903ab7072084ca2a7c10fb0e3f8f
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-03-12 18:52:55 +0100

    Update badges.js

--- javascripts/discourse/components/badges.js
+++ javascripts/discourse/components/badges.js
@@ -1,5 +1,6 @@
 import Component from "@ember/component";
 import CardContentsBase from "discourse/mixins/card-contents-base";
+import discourseComputed, { observes } from "discourse-common/utils/decorators";
 import User from "discourse/models/user";
 
 export default Component.extend({
