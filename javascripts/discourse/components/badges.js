133795e795924489db4121efd783670fa8e48a84
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-03-12 18:48:18 +0100

    Update badges.js

--- javascripts/discourse/components/badges.js
+++ javascripts/discourse/components/badges.js
@@ -2,7 +2,7 @@ import Component from "@ember/component";
 import CardContentsBase from "discourse/mixins/card-contents-base";
 import User from "discourse/models/user";
 
-export default Component.extend({
+export default Component.extend(CardContentsBase, {
     classNameBindings: [
     "visible:show",
     "showBadges",
