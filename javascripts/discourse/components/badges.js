28b226d28be9cff4b4e74f0b7d3893db66820305
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-03-12 18:45:18 +0100

    Create badges.js

---
+++ javascripts/discourse/components/badges.js
@@ -0,0 +1,13 @@
+import Component from "@ember/component";
+import CardContentsBase from "discourse/mixins/card-contents-base";
+import User from "discourse/models/user";
+
+export default Component.extend({
+  tagName: "",
+  
+  init() {
+    this._super(...arguments);
+    @discourseComputed("user.badge_count", "user.featured_user_badges.length")
+    moreBadgesCount: (badgeCount, badgeLength) => badgeCount - badgeLength,
+  }
+})
