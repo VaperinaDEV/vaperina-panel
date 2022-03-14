e544e5235f4b9dddcf2f2e9ab260a64eb41ac5d2
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-03-14 10:46:54 +0100

    Update badges.js

--- javascripts/discourse/templates/connectors/discovery-list-container-top/badges.js
+++ javascripts/discourse/templates/connectors/discovery-list-container-top/badges.js
@@ -1,14 +1,15 @@
-import Component from "@ember/component";
-import CardContentsBase from "discourse/mixins/card-contents-base";
+import Controller, { inject as controller } from "@ember/controller";
+import I18n from "I18n";
+import { alias } from "@ember/object/computed";
+import bootbox from "bootbox";
+import { exportUserArchive } from "discourse/lib/export-csv";
 import discourseComputed, { observes } from "discourse-common/utils/decorators";
-import User from "discourse/models/user";
 
-export default Component.extend({
-    classNameBindings: [
-    "visible:show",
-    "showBadges",
-  ],
-
-  @discourseComputed("user.badge_count", "user.featured_user_badges.length")
-  moreBadgesCount: (badgeCount, badgeLength) => badgeCount - badgeLength,
+export default Controller.extend({
+  @discourseComputed("currentUser.draft_count")
+  draftLabel(count) {
+    return count > 0
+      ? I18n.t("drafts.label_with_count", { count })
+      : I18n.t("drafts.label");
+  },
 });
